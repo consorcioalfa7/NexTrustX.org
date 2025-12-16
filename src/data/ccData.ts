ddb8fcb848bb5f366dd0b10e864248380a4e5561
iahub360-stack <iahub360@gmail.com>
2025-12-16 13:38:52 -0300

    Atualização CC Consul16122025

--- src/data/ccData.ts
+++ src/data/ccData.ts
@@ -24,7 +24,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "50.811,55", 
     limiteTotal: "102.570,00", 
-    preco: "450,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -36,7 +36,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "41.824,84", 
     limiteTotal: "45.000,00", 
-    preco: "400,00", 
+    preco: "425,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -48,7 +48,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "57.934,43", 
     limiteTotal: "82.530,00", 
-    preco: "450,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -60,7 +60,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "42.299,18", 
     limiteTotal: "50.500,00", 
-    preco: "400,00", 
+    preco: "425,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -72,7 +72,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "29.300,52", 
     limiteTotal: "45.000,00", 
-    preco: "350,00", 
+    preco: "375,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -84,7 +84,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "112.612,95", 
     limiteTotal: "121.390,00", 
-    preco: "450,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -96,19 +96,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "80.313,26", 
     limiteTotal: "81.000,00", 
-    preco: "450,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "523431", 
-    banco: "ITAU UNIBANCO, S.A.", 
-    level: "BLACK", 
-    cpf: "SEM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "20.968,54", 
-    limiteTotal: "22.400,00", 
-    preco: "300,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -120,19 +108,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "40.413,76", 
     limiteTotal: "41.160,00", 
-    preco: "350,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "553636", 
-    banco: "ITAU UNIBANCO, S.A.", 
-    level: "BLACK", 
-    cpf: "SEM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "41.150,87", 
-    limiteTotal: "54.500,00", 
-    preco: "350,00", 
+    preco: "375,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -144,31 +120,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "42.371,24", 
     limiteTotal: "48.660,00", 
-    preco: "350,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "553665", 
-    banco: "ITAU UNIBANCO, S.A.", 
-    level: "BLACK", 
-    cpf: "COM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "51.060,65", 
-    limiteTotal: "59.620,00", 
-    preco: "450,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "549167", 
-    banco: "ITAU UNIBANCO, S.A.", 
-    level: "PLATINUM", 
-    cpf: "SEM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "37.662,48", 
-    limiteTotal: "40.000,00", 
-    preco: "350,00", 
+    preco: "375,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -180,7 +132,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "39.110,89", 
     limiteTotal: "50.000,00", 
-    preco: "350,00", 
+    preco: "375,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -192,19 +144,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "35.346,02", 
     limiteTotal: "66.700,00", 
-    preco: "330,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "477176", 
-    banco: "ITAU UNIBANCO HOLDING, S.A.", 
-    level: "INFINITE", 
-    cpf: "SEM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "24.051,57", 
-    limiteTotal: "35.000,00", 
-    preco: "300,00", 
+    preco: "355,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -216,7 +156,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "55.826,58", 
     limiteTotal: "94.450,00", 
-    preco: "450,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -228,7 +168,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "86.656,01", 
     limiteTotal: "91.000,00", 
-    preco: "450,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -240,7 +180,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "38.655,56", 
     limiteTotal: "45.000,00", 
-    preco: "330,00", 
+    preco: "355,00", 
     pais: "Brasil",
     status: "ativo"
   },
@@ -252,19 +192,7 @@ export const ccData: CCConsultavel[] = [
     nome: "COM NOME", 
     limiteDisponivel: "109.808,39", 
     limiteTotal: "109.920,00", 
-    preco: "450,00", 
-    pais: "Brasil",
-    status: "ativo"
-  },
-  { 
-    bin: "444054", 
-    banco: "ITAU UNIBANCO HOLDING, S.A.", 
-    level: "GOLD", 
-    cpf: "SEM CPF", 
-    nome: "COM NOME", 
-    limiteDisponivel: "35.088,75", 
-    limiteTotal: "53.200,00", 
-    preco: "300,00", 
+    preco: "475,00", 
     pais: "Brasil",
     status: "ativo"
   }
